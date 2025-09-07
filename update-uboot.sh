09785421b2c6ecf9c1e7177a9b45842db12a3b05
nawres <nawresbarhoumi2019@gmail.com>
2025-09-07 16:11:03 +0100

    Revert "update the script"
    This reverts commit 54e7311ba137c5697a1662ddd4ce7ea0df540944.

--- update-uboot.sh
+++ update-uboot.sh
@@ -5,37 +5,25 @@ UBOOT_SRC=u-boot/u-boot
 UBOOT_DES=files/u-boot
 BOOT_MNT=boot
 
+# 1.create a loop device 
 
-do_losetup(){
-    # 1.create a loop device 
+loop_dev=$(sudo losetup -f --show --partscan ${SDIMG})
+echo "loop device created :${loop_dev}"
 
-    loop_dev=$(sudo losetup -f --show --partscan ${SDIMG})
-    echo "loop device created :${loop_dev}"
-}
+# 2.mount sd.img 
+loop_dev_p1=${loop_dev}p1
+echo "mounting ${loop_dev_p1} to ${BOOT_MNT}"
+sudo mount ${loop_dev_p1} ${BOOT_MNT}
 
-do_mount-boot(){
-    # 2.mount sd.img 
-    loop_dev_p1=${loop_dev}p1
-    echo "mounting ${loop_dev_p1} to ${BOOT_MNT}"
-    sudo mount ${loop_dev_p1} ${BOOT_MNT}
-}
+# 3.update u-boot
 
-do_update(){
-    # 3.update u-boot
+rm ${UBOOT_DES}
+cp ${UBOOT_SRC} ${UBOOT_DES}
 
-    rm ${UBOOT_DES}
-    cp ${UBOOT_SRC} ${UBOOT_DES}
-}
+#4.umount 
 
+sudo umount ${BOOT_MNT}
 
-do_umount(){
-    #4.umount 
-    sudo umount ${BOOT_MNT}
-
-}
-
-do_detach(){
-    #5.detach loop device 
-    echo "detaching : ${loop_dev}"
-    sudo losetup -d ${loop_dev}
-}
+#5.detach loop device 
+echo "detaching : ${loop_dev}"
+sudo losetup -d ${loop_dev}
