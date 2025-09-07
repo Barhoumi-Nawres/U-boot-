eb8ec1395d84db9c14c58cd24cb5cea2fdae682c
nawres <nawresbarhoumi2019@gmail.com>
2025-09-07 02:45:59 +0100

    this script is about to create a loop device for sd image mount boot partition update u-boot detach loop device
    Signed-off-by: nawres <nawresbarhoumi2019@gmail.com>

---
+++ update-uboot.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+SDIMG=sd.img
+UBOOT_SRC=u-boot/u-boot
+UBOOT_DES=files/u-boot
+BOOT_MNT=boot
+
+# 1.create a loop device 
+
+loop_dev=$(sudo losetup -f --show --partscan ${SDIMG})
+echo "loop device created :${loop_dev}"
+
+# 2.mount sd.img 
+loop_dev_p1=${loop_dev}p1
+echo "mounting ${loop_dev_p1} to ${BOOT_MNT}"
+sudo mount ${loop_dev_p1} ${BOOT_MNT}
+
+# 3.update u-boot
+
+rm ${UBOOT_DES}
+cp ${UBOOT_SRC} ${UBOOT_DES}
+
+#4.umount 
+
+sudo umount ${BOOT_MNT}
+
+#5.detach loop device 
+echo "detaching : ${loop_dev}"
+sudo losetup -d ${loop_dev}
