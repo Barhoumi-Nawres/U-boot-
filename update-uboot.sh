54e7311ba137c5697a1662ddd4ce7ea0df540944
nawres <nawresbarhoumi2019@gmail.com>
2025-09-07 04:07:47 +0100

    update the script

--- update-uboot.sh
+++ update-uboot.sh
@@ -5,25 +5,37 @@ UBOOT_SRC=u-boot/u-boot
 UBOOT_DES=files/u-boot
 BOOT_MNT=boot
 
-# 1.create a loop device 
 
-loop_dev=$(sudo losetup -f --show --partscan ${SDIMG})
-echo "loop device created :${loop_dev}"
+do_losetup(){
+    # 1.create a loop device 
 
-# 2.mount sd.img 
-loop_dev_p1=${loop_dev}p1
-echo "mounting ${loop_dev_p1} to ${BOOT_MNT}"
-sudo mount ${loop_dev_p1} ${BOOT_MNT}
+    loop_dev=$(sudo losetup -f --show --partscan ${SDIMG})
+    echo "loop device created :${loop_dev}"
+}
 
-# 3.update u-boot
+do_mount-boot(){
+    # 2.mount sd.img 
+    loop_dev_p1=${loop_dev}p1
+    echo "mounting ${loop_dev_p1} to ${BOOT_MNT}"
+    sudo mount ${loop_dev_p1} ${BOOT_MNT}
+}
 
-rm ${UBOOT_DES}
-cp ${UBOOT_SRC} ${UBOOT_DES}
+do_update(){
+    # 3.update u-boot
 
-#4.umount 
+    rm ${UBOOT_DES}
+    cp ${UBOOT_SRC} ${UBOOT_DES}
+}
 
-sudo umount ${BOOT_MNT}
 
-#5.detach loop device 
-echo "detaching : ${loop_dev}"
-sudo losetup -d ${loop_dev}
+do_umount(){
+    #4.umount 
+    sudo umount ${BOOT_MNT}
+
+}
+
+do_detach(){
+    #5.detach loop device 
+    echo "detaching : ${loop_dev}"
+    sudo losetup -d ${loop_dev}
+}
